9c8340475929b6370a6ee1423f5bc70604792f0b
Krister Shalm <kshalm@hey.com>
2025-05-29 21:46:39 -0600

    replaced missing functions

--- src/detector_server.py
+++ src/detector_server.py
@@ -17,41 +17,185 @@ def save_yaml_data(config_file, yaml_data):
     with open(filepath, 'w') as f:
         yaml.dump(yaml_data, f)
 
-def connect_to_keithly(config_file):
-    yaml_data = load_yaml(config_file)
-    gpib = yaml_data['Keithley']['Info']['GpibAddr']
-    # try:
-    kc = keithley(gpib)
-    return kc, True
-    # except Exception:
-    #     return None, False
+# def connect_to_keithly(config_file):
+#     yaml_data = load_yaml(config_file)
+#     gpib = yaml_data['Keithley']['Info']['GpibAddr']
+#     # try:
+#     kc = keithley(gpib)
+#     return kc, True
+#     # except Exception:
+#     #     return None, False
+
+# def connect_to_mcc():
+#     # try:
+#     m = mccusb3100()
+#     return m, True
+#     # except Exception:
+#     #     return None, False
+
+# def reset_comparator(yaml_file):
+#     if not DetectorControlService.mccPresent:
+#         return False
+#     cfg = load_yaml(yaml_file)
+#     try:
+#         ok = DetectorControlService.mcc.configV(cfg["Comparator"])
+#         return bool(ok)
+#     except Exception:
+#         return False
+
+# def reset_detectors(yaml_file):
+#     if not DetectorControlService.biasControlPresent:
+#         return False
+#     cfg = load_yaml(yaml_file)
+#     try:
+#         ok = DetectorControlService.biasControl.resetConfig(cfg["Keithley"]["Bias"])
+#         return bool(ok)
+#     except Exception:
+#         return False
+
+def connect_to_keithly(configFile):
+    yamlData = load_yaml(configFile)
+    gpibaddr = yamlData['Keithley']['Info']['GpibAddr']
+    try:
+        biasControl = keithley(gpibaddr)
+        biasControlPresent = True
+    except:
+        biasControl = None
+        biasControlPresent = False
+    return(biasControl, biasControlPresent)
 
 def connect_to_mcc():
-    # try:
-    m = mccusb3100()
-    return m, True
-    # except Exception:
-    #     return None, False
-
-def reset_comparator(yaml_file):
-    if not DetectorControlService.mccPresent:
-        return False
-    cfg = load_yaml(yaml_file)
     try:
-        ok = DetectorControlService.mcc.configV(cfg["Comparator"])
-        return bool(ok)
-    except Exception:
-        return False
+        mcc = mccusb3100()
+        print(mcc.identify())
+        mccPresent = True
+    except:
+        mcc = None
+        mccPresent = False
+    return(mcc, mccPresent)
 
-def reset_detectors(yaml_file):
-    if not DetectorControlService.biasControlPresent:
-        return False
-    cfg = load_yaml(yaml_file)
+# def load_yaml(filename = 'det.yaml'):
+#     path = os.path.join(os.path.pardir, "config/")
+#     # print(path)
+#     filepath = os.path.join(path, filename)
+
+#     with open(filepath) as f:
+#         unorderedYamlData = yaml.safe_load(f)
+#     return(unorderedYamlData)
+
+# def save_yaml_data(configFile, yamlData):
+#     path = os.path.join(os.path.pardir, "config/")
+#     filepath = os.path.join(path, configFile)
+#     print('filepath to write', filepath)
+#     print(yamlData)
+#     with open(filepath, 'w') as f:
+#         yaml.dump(yamlData, f)
+
+
+def set_comparator_channel(channelName, value, yamlFile = 'det.yaml'):
+    """Sets comparator channels javascript style"""
+
+    if mccPresent == False:
+        return(False)
+    yamlData = load_yaml(yamlFile)
+    yamlData["Comparator"][channelName]['value'] = float(value)
+    save_yaml_data(yamlFile, yamlData)
+
+    success = reset_comparator(yamlFile)
+    return(success)
+
+def set_all_comparator_channels(comparatorSettings, yamlFile='det.yaml'):
+    """Sets comparator channels javascript style"""
+
+    if mccPresent == False:
+        return(False)
+
+    yamlData = load_yaml(yamlFile)
+
+    for channelName, value in comparatorSettings.items():
+        yamlData["Comparator"][channelName]['value'] = float(value)
+
+    save_yaml_data(yamlFile, yamlData)
+    success = reset_comparator(yamlFile)
+    return(success)
+
+def reset_comparator(yamlFile):
+    if mccPresent == False:
+        return(False)
+
+    yamlData = load_yaml(yamlFile)
+    success = True
     try:
-        ok = DetectorControlService.biasControl.resetConfig(cfg["Keithley"]["Bias"])
-        return bool(ok)
-    except Exception:
-        return False
+        result = mcc.configV(yamlData["Comparator"])
+        if result:
+            print('Comparator (MCC) Channels set')
+        else:
+            print('Comparator (MCC) Error in setting voltages.')
+            success = False
+    except Exception as e:
+        msg = "ERROR setting comparator channels %r" % e
+        print(msg)
+        success = False
+    return(success)
+
+def set_detector_channel(ch, voltage, yamlFile = 'det.yaml'):
+    """Sets detector (Keithley213) channels """
+    if biasControlPresent == False:
+        return(False)
+    yamlData = load_yaml(yamlFile)
+    yamlData["Keithley"]["Bias"][ch] = float(voltage)
+    save_yaml_data(yamlFile, yamlData)
+    print(yamlData["Keithley"])
+    success = reset_detectors(yamlFile)
+    return(success)
+
+def set_detector_voltage(ch, voltage, yamlFile = 'det.yaml'):
+    success = biasControl.set_voltage(ch, voltage)
+    if success:
+        yamlData = load_yaml(yamlFile)
+        yamlData["Keithley"]["Bias"][ch] = float(voltage)
+        save_yaml_data(yamlFile, yamlData)
+    return success
+
+def set_all_detector_channels(biasSettings, yamlFile = 'det.yaml'):
+    """Sets detector (Keithley213) channels """
+    if biasControlPresent == False:
+        return(False)
+
+    yamlData = load_yaml(yamlFile)
+
+    for ch, value in biasSettings.items():
+        print(ch, value)
+        yamlData["Keithley"]["Bias"][ch] = float(value)
+    save_yaml_data(yamlFile, yamlData)
+
+    success = reset_detectors(yamlFile)
+    return(success)
+
+def reset_detectors(yamlFile = 'det.yaml'):
+    if biasControlPresent == False:
+        return(False)
+
+    yamlData = load_yaml(yamlFile)
+    success = True
+
+    try:
+        result = biasControl.resetConfig(yamlData["Keithley"]["Bias"])
+
+        if result:
+            print("Keithley Channels Reset!")
+        else:
+            print("Keithley ERROR!")
+            success = False
+    except NameError as e:
+        msg= "ERROR setting detector channels. Is Keithley plugged in? %r " % e
+        print(msg)
+        success = False
+    except Exception as e:
+        print("ERROR setting detector channels %r" % e)
+        success = False
+    return(success)
+
 
 class DetectorControlService(ZMQServiceBase):
     def __init__(self, config_file='det.yaml', n_workers=1):
@@ -81,18 +225,18 @@ class DetectorControlService(ZMQServiceBase):
         self.logger.info(f"Initializing {self.service_name} with config: {self.config}")
         print(f"Config file: {self.config}")
         
-        # establish hardware connections
-        # try:
-        self.biasControl, self.biasControlPresent = connect_to_keithly(self.config_file)
-        self.logger.info(f"Keithley Bias Control initialized: {self.biasControlPresent}")
-        print(f"Keithley Bias Control initialized: {self.biasControlPresent}")
-        # except Exception as e:
-            # self.logger.error(f"Failed to connect to Keithley Bias Control: {e}")
-            # os._exit(1)
-        # try:
-        self.mcc, self.mccPresent = connect_to_mcc()
-        self.logger.info(f"MCC USB-3100 initialized: {self.mccPresent}")
-        print(f"MCC USB-3100 initialized: {self.mccPresent}")
+        # # establish hardware connections
+        # # try:
+        # self.biasControl, self.biasControlPresent = connect_to_keithly(self.config_file)
+        # self.logger.info(f"Keithley Bias Control initialized: {self.biasControlPresent}")
+        # print(f"Keithley Bias Control initialized: {self.biasControlPresent}")
+        # # except Exception as e:
+        #     # self.logger.error(f"Failed to connect to Keithley Bias Control: {e}")
+        #     # os._exit(1)
+        # # try:
+        # self.mcc, self.mccPresent = connect_to_mcc()
+        # self.logger.info(f"MCC USB-3100 initialized: {self.mccPresent}")
+        # print(f"MCC USB-3100 initialized: {self.mccPresent}")
         # except Exception as e:
         #     self.logger.error(f"Failed to connect to MCC USB-3100: {e}")
         #     os._exit(1)
@@ -186,6 +330,12 @@ class DetectorControlService(ZMQServiceBase):
         #     return f"Error: {e}"
 
 if __name__ == '__main__':
+    configFile = 'det.yaml'  # Assuming the file is named 'det.yaml' in the config directory
+    yamlData = load_yaml(configFile)
+    print(yamlData)
+    print('')
+    biasControl, biasControlPresent = connect_to_keithly(configFile)
+    mcc, mccPresent = connect_to_mcc()
+    print(biasControl.identify())
     service = DetectorControlService(config_file='det.yaml', n_workers=1)
     service.start()   # blocks until SIGINT/SIGTERMworkers=1)
-    service.start()   # blocks until SIGINT/SIGTERM
